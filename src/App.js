ca813feeee84162f116e1d0c6df07a6469f40bde
Reddy531 <chakradhar.lsg4@gmail.com>
2020-12-21 03:16:31 -0600

    Added Pagination component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Usercomponent from "./Usercomponent"
+import Pagination from "react-pagination-js";
+import "react-pagination-js/dist/styles.css";
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    currentPage: 1,
+    api_response: []
+  };
+
+  apicall = async (page) => {
+    await axios.get(`https://api.github.com/users?since=${page}`)
+      .then(response => {
+        this.setState({ ...this.state, api_response: response.data });
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+  }
+  componentDidMount() {
+    this.apicall(this.state.currentPage);
+  }
+  handleChange = numPage => {
+    this.setState({ currentPage: numPage });
+    this.apicall(numPage);
+  };
+  render() {
+    return (
+      <div>
+        <h2>Front End Developer Take Home Assessment</h2>
+        <Usercomponent data={this.state.api_response} />
+        <Pagination
+          currentPage={this.state.currentPage}
+          totalSize={100}
+          changeCurrentPage={this.handleChange}
+          theme="bootstrap"
+        />
+        <h2>current Page:{this.state.currentPage}</h2>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/Usercomponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+class Usercomponent extends Component {
+    render() {
+        return (
+            <div>
+                Hello User
+            </div>
+        );
+    }
+}
+export default Usercomponent;
