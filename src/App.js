bed3c7c2e76688d3da592d5ba89ae1267ae6e738
Reddy531 <chakradhar.lsg4@gmail.com>
2020-12-23 02:03:58 -0600

    Added infinite scroll with API call

--- src/App.js
+++ src/App.js
@@ -1,47 +1,11 @@
 import React from "react";
 import Usercomponent from "./Usercomponent"
-import Pagination from "react-pagination-js";
-import "react-pagination-js/dist/styles.css";
-import axios from 'axios';
-
 class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentPage: 1,
-      api_response: []
-    };
-  }
-
-  apicall = async (page) => {
-    await axios.get(`https://api.github.com/users?since=${page}`)
-      .then(response => {
-        this.setState({ ...this.state, api_response: response.data });
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
-  }
-  componentDidMount() {
-    this.apicall(this.state.currentPage);
-  }
-  handleChange = async (numPage) => {
-    await this.setState({ currentPage: numPage, api_response: [] });
-    this.apicall(this.state.currentPage);
-  };
   render() {
     return (
       <div>
         <h2>Front End Developer Take Home Assessment</h2>
-        <Pagination
-          currentPage={this.state.currentPage}
-          totalSize={100}
-          changeCurrentPage={this.handleChange}
-          theme="bootstrap"
-        />
-        {this.state.api_response && this.state.api_response.length > 0 &&
-          <Usercomponent userdata={this.state.api_response} />}
+        <Usercomponent />
       </div>
     );
   }
--- src/Usercomponent.js
+++ src/Usercomponent.js
@@ -1,18 +1,52 @@
 import React, { Component } from "react";
-import './index.css';
+import axios from "axios";
 
 class Usercomponent extends Component {
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
         this.state = {
             userdata: [],
             sorted_data: [],
-            search: ""
-        }
+            loading: false,
+            page: 0,
+            prevY: 0
+        };
     }
-    async componentDidMount() {
-        const { userdata } = this.props;
-        await this.setState({ ...this.state, userdata: userdata, sorted_data: userdata })
+    componentDidMount() {
+        this.getUserdata(this.state.page);
+
+        var options = {
+            root: null,
+            rootMargin: "0px",
+            threshold: 1.0
+        };
+
+        this.observer = new IntersectionObserver(
+            this.handleObserver.bind(this),
+            options
+        );
+        this.observer.observe(this.loadingRef);
+    }
+    handleObserver(entities, observer) {
+        const y = entities[0].boundingClientRect.y;
+        if (this.state.prevY > y) {
+            const lastUser = this.state.userdata[this.state.userdata.length - 1];
+            const currentUserPage = lastUser.albumId;
+            this.getUserdata(currentUserPage);
+            this.setState({ page: currentUserPage });
+        }
+        this.setState({ prevY: y });
+    }
+    async getUserdata(page) {
+        this.setState({ loading: true });
+        axios
+            .get(
+                `https://api.github.com/users?since=${page}`
+            )
+            .then(res => {
+                this.setState({ userdata: [...this.state.userdata, ...res.data], sorted_data: [...this.state.sorted_data, ...res.data] });
+                this.setState({ loading: false });
+            });
     }
     filterData = async (e) => {
         var x = -1;
@@ -28,57 +62,62 @@ class Usercomponent extends Component {
         });
         await this.setState({ ...this.state, sorted_data: updatedList });
     };
-    onRowClick = (record) => {
-        console.log("Row Clicked" + record.login)
-    }
     render() {
+        const loadingCSS = {
+            height: "100px",
+            margin: "30px"
+        };
+        const loadingTextCSS = { display: this.state.loading ? "block" : "none" };
         return (
-            <div>
-                {this.state.sorted_data &&
-                    <div>
-                        <h2 className="leftmargin">List of Users</h2>
-                        {this.state.userdata && this.state.userdata.length > 0 && <input
-                            className="input" type="text"
-                            id="search"
-                            placeholder="Search users..."
-                            onChange={(value) => this.filterData(value)}
-                        />}
-                        <table className="detail_table">
-                            <thead>
-                                <tr>
-                                    <th><h3>User Avatar</h3></th>
-                                    <th><h3>User Login</h3></th>
-                                </tr>
-                            </thead>
-                            {this.state.sorted_data && this.state.sorted_data.length > 0 && this.state.sorted_data.map((record, index) => {
-                                return (
-                                    <tbody>
-                                        <tr key={index} onClick={() => this.onRowClick(record)}>
-                                            <td className="table_data">
-                                                <img
-                                                    style={{ width: "30%", height: "30%" }}
-                                                    src={record.avatar_url}
-                                                    alt="avatar"
-                                                >
-                                                </img>
-                                            </td>
-                                            <td className="table_data"><h3>{record.login}</h3></td>
-                                        </tr>
-                                    </tbody>
-                                );
-                            })
-                            }
-                            {this.state.sorted_data && this.state.sorted_data.length === 0 &&
+            <div className="container">
+                <div style={{ minHeight: "800px" }}>
+                    <h2 className="leftmargin">List of Users</h2>
+                    {this.state.userdata && this.state.userdata.length > 0 && <input
+                        className="input" type="text"
+                        id="search"
+                        placeholder="Search users..."
+                        onChange={(value) => this.filterData(value)}
+                    />}
+                    <table className="detail_table">
+                        <thead>
+                            <tr>
+                                <th><h3>User Avatar</h3></th>
+                                <th><h3>User Login</h3></th>
+                            </tr>
+                        </thead>
+                        {this.state.sorted_data && this.state.sorted_data.length > 0 && this.state.sorted_data.map((record, index) => {
+                            return (
                                 <tbody>
-                                    <tr><td colSpan="2" className="table_data">No records found for the search criteria</td></tr>
+                                    <tr key={index} onClick={() => this.onRowClick(record)}>
+                                        <td className="table_data">
+                                            <img
+                                                style={{ width: "30%", height: "30%" }}
+                                                src={record.avatar_url}
+                                                alt="avatar"
+                                            >
+                                            </img>
+                                        </td>
+                                        <td className="table_data"><h3>{record.login}</h3></td>
+                                    </tr>
                                 </tbody>
-                            }
-                        </table>
-                    </div>
-                }
-
+                            );
+                        })}
+                        {this.state.sorted_data && this.state.sorted_data.length === 0 &&
+                            <tbody>
+                                <tr><td colSpan="2" className="table_data">No records found for the search criteria</td></tr>
+                            </tbody>
+                        }
+                    </table>
+                </div>
+                <div
+                    ref={loadingRef => (this.loadingRef = loadingRef)}
+                    style={loadingCSS}
+                >
+                    <span style={loadingTextCSS}>Loading...</span>
+                </div>
             </div>
         );
     }
 }
+
 export default Usercomponent;
