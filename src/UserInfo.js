746b28623e28fb1abece9baacff7ecf7367d75bf
Reddy531 <chakradhar.lsg4@gmail.com>
2020-12-23 03:00:07 -0600

    Added User Info component with repos and followers informatoon

---
+++ src/UserInfo.js
@@ -0,0 +1,84 @@
+import React from "react";
+import axios from "axios";
+
+class UserInfo extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            repoInfo: [],
+            followersInfo: []
+        };
+    }
+    async componentDidMount() {
+        const { userinfo } = this.props;
+        document.title = 'UserInformation';
+        await axios.get(userinfo.repos_url)
+            .then(response => {
+                this.setState({ ...this.state, repoInfo: response.data });
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+        await axios.get(userinfo.followers_url)
+            .then(response => {
+                this.setState({ ...this.state, followersInfo: response.data });
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+    render() {
+        const { userinfo } = this.props;
+        return (
+            <div>
+                <h3>User Repositories Information</h3>
+                {userinfo &&
+                    <table className="info_table">
+                        <thead>
+                            <tr>
+                                <th><h3>Repo ID</h3></th>
+                                <th><h3>Repo Name</h3></th>
+                                <th><h3>Repo Full Name</h3></th>
+                            </tr>
+                        </thead>
+                        {this.state.repoInfo && this.state.repoInfo.length > 0 && this.state.repoInfo.map((record, index) => {
+                            return (
+                                <tbody>
+                                    <tr key={index}>
+                                        <td className="table_data"><h3>{record.id}</h3></td>
+                                        <td className="table_data"><h3>{record.name}</h3></td>
+                                        <td className="table_data"><h3>{record.full_name}</h3></td>
+                                    </tr>
+                                </tbody>
+                            );
+                        })}
+                    </table>
+                }
+                <h3>User Followers Information</h3>
+                {userinfo &&
+                    <table className="info_table">
+                        <thead>
+                            <tr>
+                                <th><h3>Follower ID</h3></th>
+                                <th><h3>Follower Name</h3></th>
+                                <th><h3>Follower URL</h3></th>
+                            </tr>
+                        </thead>
+                        {this.state.followersInfo && this.state.followersInfo.length > 0 && this.state.followersInfo.map((record, index) => {
+                            return (
+                                <tbody>
+                                    <tr key={index}>
+                                        <td className="table_data"><h3>{record.id}</h3></td>
+                                        <td className="table_data"><h3>{record.login}</h3></td>
+                                        <td className="table_data"><h3>{record.url}</h3></td>
+                                    </tr>
+                                </tbody>
+                            );
+                        })}
+                    </table>
+                }
+            </div>
+        );
+    }
+}
+export default UserInfo;
--- src/Usercomponent.js
+++ src/Usercomponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
-
+import UserInfo from "./UserInfo"
 class Usercomponent extends Component {
     constructor() {
         super();
@@ -8,19 +8,20 @@ class Usercomponent extends Component {
             userdata: [],
             sorted_data: [],
             loading: false,
+            userInfoRecord: {},
+            showDetail: false,
             page: 0,
             prevY: 0
         };
     }
     componentDidMount() {
+        document.title = 'UsersList';
         this.getUserdata(this.state.page);
-
         var options = {
             root: null,
             rootMargin: "0px",
             threshold: 1.0
         };
-
         this.observer = new IntersectionObserver(
             this.handleObserver.bind(this),
             options
@@ -60,8 +61,16 @@ class Usercomponent extends Component {
                 x !== -1
             );
         });
-        await this.setState({ ...this.state, sorted_data: updatedList });
+        const removeDuplicatesFromArray = (updatedList) => [...new Set(
+            updatedList.map(el => JSON.stringify(el))
+        )].map(e => JSON.parse(e));
+
+        await this.setState({ ...this.state, sorted_data: removeDuplicatesFromArray(updatedList) });
     };
+    handleRecordClick = async (record, e) => {
+        e.preventDefault();
+        await this.setState({ ...this.state, userInfoRecord: record, showDetail: true });
+    }
     render() {
         const loadingCSS = {
             height: "100px",
@@ -71,43 +80,50 @@ class Usercomponent extends Component {
         return (
             <div className="container">
                 <div style={{ minHeight: "800px" }}>
-                    <h2 className="leftmargin">List of Users</h2>
-                    {this.state.userdata && this.state.userdata.length > 0 && <input
-                        className="input" type="text"
-                        id="search"
-                        placeholder="Search users..."
-                        onChange={(value) => this.filterData(value)}
-                    />}
-                    <table className="detail_table">
-                        <thead>
-                            <tr>
-                                <th><h3>User Avatar</h3></th>
-                                <th><h3>User Login</h3></th>
-                            </tr>
-                        </thead>
-                        {this.state.sorted_data && this.state.sorted_data.length > 0 && this.state.sorted_data.map((record, index) => {
-                            return (
+                    {this.state.showDetail &&
+                        <UserInfo userinfo={this.state.userInfoRecord} />
+                    }
+                    {!this.state.showDetail && <div>
+                        <h2 className="leftmargin">List of Users</h2>
+                        {this.state.userdata && this.state.userdata.length > 0 && <input
+                            className="input" type="text"
+                            id="search"
+                            placeholder="Search users..."
+                            onChange={(value) => this.filterData(value)}
+                        />}
+                        <table className="detail_table">
+                            <thead>
+                                <tr>
+                                    <th><h3>User Avatar</h3></th>
+                                    <th><h3>User Login</h3></th>
+                                </tr>
+                            </thead>
+                            {this.state.sorted_data && this.state.sorted_data.length > 0 && this.state.sorted_data.map((record, index) => {
+                                return (
+                                    <tbody>
+                                        <tr key={record.id}>
+                                            <td className="table_data">
+                                                <img
+                                                    style={{ width: "30%", height: "30%" }}
+                                                    src={record.avatar_url}
+                                                    alt="avatar"
+                                                >
+                                                </img>
+                                            </td>
+                                            <td className="table_data"><h3>
+                                                <a href="/user_info" onClick={(e) => this.handleRecordClick(record, e)}>{record.login}</a>
+                                            </h3></td>
+                                        </tr>
+                                    </tbody>
+                                );
+                            })}
+                            {this.state.sorted_data && this.state.sorted_data.length === 0 &&
                                 <tbody>
-                                    <tr key={index} onClick={() => this.onRowClick(record)}>
-                                        <td className="table_data">
-                                            <img
-                                                style={{ width: "30%", height: "30%" }}
-                                                src={record.avatar_url}
-                                                alt="avatar"
-                                            >
-                                            </img>
-                                        </td>
-                                        <td className="table_data"><h3>{record.login}</h3></td>
-                                    </tr>
+                                    <tr key="1"><td colSpan="2" className="table_data">No records found for the search criteria</td></tr>
                                 </tbody>
-                            );
-                        })}
-                        {this.state.sorted_data && this.state.sorted_data.length === 0 &&
-                            <tbody>
-                                <tr><td colSpan="2" className="table_data">No records found for the search criteria</td></tr>
-                            </tbody>
-                        }
-                    </table>
+                            }
+                        </table>
+                    </div>}
                 </div>
                 <div
                     ref={loadingRef => (this.loadingRef = loadingRef)}
